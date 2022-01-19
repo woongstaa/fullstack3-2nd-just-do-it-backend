6dfc6e98f6d65601c61e127e73ec954e5e748631
LJHyeokk <clzlsqkq56@naver.com>
2022-01-20 02:08:03 +0900

    [추가] - detail

--- models/productDao.js
+++ models/productDao.js
@@ -2,6 +2,42 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const updateQuantity = async () => {};
+const getProductData = async style_code => {
+  return await prisma.$queryRaw`
+    SELECT
+      products.style_code,
+      products.name,
+      categories.name as category,
+      product_colors.name as color,
+      product_colors.color_hex as hex,
+      product_genders.name as gender,
+      normal_price,
+      sale_rate,
+      sale_price,
+      is_member,
+      sub_icon.name as shoes_type,
+      sub_brand.name as brand,
+      sub_clothes.name as clothes_type,
+      sub_accessories.name as acc_type
+    FROM
+      products
+    JOIN
+      categories ON category_id = categories.id
+    JOIN
+      product_colors ON color_id = product_colors.id
+    JOIN
+      product_genders ON gender_id = product_genders.id
+    JOIN
+      sub_icon ON sub_icon_id = sub_icon.id
+    JOIN
+      sub_brand ON sub_brand_id = sub_brand.id
+    JOIN
+      sub_clothes ON sub_clothes_id = sub_clothes.id
+    JOIN
+      sub_accessories ON sub_accessories_id = sub_accessories.id
+    WHERE
+      products.style_code = "AAA-0001";
+  `;
+};
 
-export default { updateQuantity };
+export default { getProductData };
