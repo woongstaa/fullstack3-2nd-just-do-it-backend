e518e6862912112badb908532a03c4c34aa7d436
GDandTOP <larry8000@nate.com>
2022-01-20 17:39:57 +0900

    데이터 추가작업 영욱

---
+++ dummy/snkrsImgAdder.sql
@@ -0,0 +1,22 @@
+INSERT INTO 
+  SNKRS_URLS (name,is_main,style_code,category_id)
+VALUES
+  ("/Images/SNKRS/1/1.png",1,"DAA-0001",4),
+  ("/Images/SNKRS/1/2.png",0,"DAA-0001",4),
+  ("/Images/SNKRS/1/3.png",0,"DAA-0001",4),
+  ("/Images/SNKRS/1/4.png",0,"DAA-0001",4),
+
+  ("/Images/SNKRS/2/1.png",1,"DAA-0002",4),
+  ("/Images/SNKRS/2/2.png",0,"DAA-0002",4),
+  ("/Images/SNKRS/2/3.png",0,"DAA-0002",4),
+  ("/Images/SNKRS/2/4.png",0,"DAA-0002",4),
+
+  ("/Images/SNKRS/3/1.png",1,"DAA-0003",4),
+  ("/Images/SNKRS/3/2.png",0,"DAA-0003",4),
+  ("/Images/SNKRS/3/3.png",0,"DAA-0003",4),
+  ("/Images/SNKRS/3/4.png",0,"DAA-0003",4),
+  
+  ("/Images/SNKRS/4/1.png",1,"DAA-0004",4),
+  ("/Images/SNKRS/4/2.png",0,"DAA-0004",4),
+  ("/Images/SNKRS/4/3.png",0,"DAA-0004",4),
+  ("/Images/SNKRS/4/4.png",0,"DAA-0004",4);
